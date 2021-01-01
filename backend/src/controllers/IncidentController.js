d0851bb443ebd212ee29243fd43c23d047f48d60
Guilherme Lameiras Fernandez <guil.fernandez1@hotmail.com>
2021-01-01 16:44:53 -0300

    Back-end: Ajustes na rota de delete de incident

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -24,28 +24,27 @@ module.exports = {
     async delete(request, response) {
         const { id } = request.params;
         const user_id = request.headers.authorization;
-
-        const incident = await Incident.findOne({
-            attributes: ['user_id'],
+        const selector = {
             where: {
-                'id': id
+                id: id
             }
-        });
+        };
 
-        console.log(user_id)
-        console.log(incident.user_id)
+        const incident = await Incident.findOne({ attributes: ['user_id'], selector })
+            .catch((error) => {
+                return response.status(404).send(error);
+            });
 
         if (incident.user_id != user_id) {
-            return response.status(401).json({ error: 'Operation not permitted.' });
+            return response.status(401).json({ error: 'Operation not permitted' });
         }
 
-        await Incident.destroy({
-            where: {
-                'id': id,
-            }
-        });
-
-        return response.status(204).send();
+        await Incident.destroy(selector)
+            .then(() => {
+                return response.status(200).send({ message: `Incident ${id} deleted` });
+            }).catch((error) => {
+                return response.status(400).send(error);
+            });
     },
 
     async update(request, response) {
@@ -61,7 +60,7 @@ module.exports = {
             }
         };
 
-        const incident = await Incident.update(data, selector)
+        await Incident.update(data, selector)
             .then(() => {
                 return response.status(200).send();
             }).catch((error) => {
