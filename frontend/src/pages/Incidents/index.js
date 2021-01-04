9ee9e064d10dd878a170f44103479aa76d9fb16d
Guilherme Lameiras Fernandez <guil.fernandez1@hotmail.com>
2021-01-03 22:30:36 -0300

    Finalização de estrutura html da página Incidents

--- frontend/src/pages/Incidents/index.js
+++ frontend/src/pages/Incidents/index.js
@@ -9,10 +9,12 @@ export default function Incidents() {
     return (
         <div className="incidents-container">
             <header>
-                <a href="https://salaodoraapp.com.br">
-                    <img src={logoImg} alt="DoraApp" />
-                </a>
-                <span>Bem-vindo, Guilherme</span>
+                <div>
+                    <a href="https://salaodoraapp.com.br">
+                        <img src={logoImg} alt="DoraApp" />
+                    </a>
+                    <span>Bem-vindo, Guilherme</span>
+                </div>
                 <Link className="button" to="/incidents/new"> Criar um incidente </Link>
             </header>
             <h1>Incidentes cadastrados</h1>
@@ -21,7 +23,7 @@ export default function Incidents() {
                     <strong>TÍTULO:</strong>
                     <p>Falta de Shampoo no estoque</p>
 
-                    <button onClick="" type="button">
+                    <button onClick="" type="button" className="edit-button">
                         <Link to="/incidents/update">
                             <FiEdit2 size={20} color="#a8a8b3" />
                         </Link>
@@ -33,7 +35,70 @@ export default function Incidents() {
                     <strong>STATUS:</strong>
                     <p>Pendente</p>
 
-                    <button onClick="" type="button">
+                    <button onClick="" type="button" className="delete-button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+
+                <li>
+                    <strong>TÍTULO:</strong>
+                    <p>Falta de Shampoo no estoque</p>
+
+                    <button onClick="" type="button" className="edit-button">
+                        <Link to="/incidents/update">
+                            <FiEdit2 size={20} color="#a8a8b3" />
+                        </Link>
+                    </button>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Estamos com falta de shampoo no estoque faz algumas semanas</p>
+
+                    <strong>STATUS:</strong>
+                    <p>Pendente</p>
+
+                    <button onClick="" type="button" className="delete-button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+
+                <li>
+                    <strong>TÍTULO:</strong>
+                    <p>Falta de Shampoo no estoque</p>
+
+                    <button onClick="" type="button" className="edit-button">
+                        <Link to="/incidents/update">
+                            <FiEdit2 size={20} color="#a8a8b3" />
+                        </Link>
+                    </button>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Estamos com falta de shampoo no estoque faz algumas semanas</p>
+
+                    <strong>STATUS:</strong>
+                    <p>Pendente</p>
+
+                    <button onClick="" type="button" className="delete-button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+
+                <li>
+                    <strong>TÍTULO:</strong>
+                    <p>Falta de Shampoo no estoque</p>
+
+                    <button onClick="" type="button" className="edit-button">
+                        <Link to="/incidents/update">
+                            <FiEdit2 size={20} color="#a8a8b3" />
+                        </Link>
+                    </button>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Estamos com falta de shampoo no estoque faz algumas semanas</p>
+
+                    <strong>STATUS:</strong>
+                    <p>Pendente</p>
+
+                    <button onClick="" type="button" className="delete-button">
                         <FiTrash2 size={20} color="#a8a8b3" />
                     </button>
                 </li>
